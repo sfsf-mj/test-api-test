3106db22d009eca340bc3e99ccddaf6d5641a287
sfsf-mj <105579075+sfsf-mj@users.noreply.github.com>
2024-11-09 20:25:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def upload_image():
         return jsonify({'message': 'ملف غير مدعوم'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
